b5a2ba12cb4fad2c56afb317303ee4875aab7eef
isabellicruz <isabellicosta2013@gmail.com>
2025-05-21 16:08:13 -0300

    pre processamento!!!

--- pre_processamento_planilhas.R
+++ pre_processamento_planilhas.R
@@ -2,13 +2,13 @@
 library(dplyr)
 
 # Caminho do arquivo CSV
-caminho_arquivo <- 'input_data/sinannet_cnv_leptobr161355200_132_11_251.csv'
+caminho_arquivo <- 'input_data/sinannet_cnv_leptobr135749200_132_11_251_2013.csv'
 
 # Verificar se o arquivo existe antes de carregar
-if(file.exists(caminho_arquivo)) {
+if (file.exists(caminho_arquivo)) 
   # Tente carregar o arquivo com o delimitador correto (vírgula ou ponto e vírgula)
-  dados <- read.csv(caminho_arquivo, fileEncoding = "ISO-8859-1", sep = ";") # Verifique se é ";"
-  
+  dados <- read.csv(caminho_arquivo, fileEncoding = "ISO-8859-1", sep = ";") 
+
   # Visualizar as primeiras linhas do arquivo
   head(dados)
   
@@ -29,7 +29,42 @@ if(file.exists(caminho_arquivo)) {
   # Exibir as tabelas no Viewer do RStudio
   View(dados_numericos)
   View(dados_caracteres)
-} else {
-  cat("O arquivo não foi encontrado. Verifique o caminho.")
-}
 
+  # Carregar pacotes necessários
+  library(dplyr)
+  
+  # Caminho do arquivo CSV
+  caminho_arquivo <- 'input_data/sinannet_cnv_leptobr135749200_132_11_251_2013.csv'
+  
+  # Verificar se o arquivo existe antes de carregar
+  if (file.exists(caminho_arquivo)) {
+    
+    # Tente carregar o arquivo com o delimitador correto (vírgula ou ponto e vírgula)
+    dados <- read.csv(caminho_arquivo, fileEncoding = "ISO-8859-1", sep = ";") 
+    
+    # Visualizar as primeiras linhas do arquivo
+    head(dados)
+    
+    # Exibir os dados no Viewer do RStudio
+    View(dados)
+    
+    # Separar colunas numéricas e de caracteres
+    dados_numericos <- dados %>%
+      select(where(is.numeric))
+    
+    dados_caracteres <- dados %>%
+      select(where(~ is.character(.) | is.factor(.)))
+    
+    # Exibir as primeiras linhas dos dados numéricos e de caracteres
+    head(dados_numericos)
+    head(dados_caracteres)
+    
+    # Exibir as tabelas no Viewer do RStudio
+    View(dados_numericos)
+    View(dados_caracteres)
+    
+  } else {
+    message("Arquivo não encontrado: ", caminho_arquivo)
+  }
+
+  
