f4777ca596542f0b1e8920eacd593305473c8f23
gandrat <gandratiagobr@gmail.comm>
2025-05-22 15:04:09 -0300

    Reorganizando scripts.

--- 00_preprocessamento.R
+++ 00_preprocessamento.R
@@ -1,13 +1,70 @@
 ##Pré processamento de dados
-
+# Carregar pacotes necessários
 library(dplyr)
-library(sf)
 
-d<-read.csv2('input_data/sinannet_cnv_leptobr161355200_132_11_251.csv',sep=';',header=TRUE,fileEncoding='iso-8859-1')
+# Caminho do arquivo CSV
+caminho_arquivo <- 'input_data/sinannet_cnv_leptobr135749200_132_11_251_2013.csv'
 
-df <- as.data.frame(sapply(d%>%select(-1), as.numeric))
+# Verificar se o arquivo existe antes de carregar
+if (file.exists(caminho_arquivo)) 
+  # Tente carregar o arquivo com o delimitador correto (vírgula ou ponto e vírgula)
+  dados <- read.csv(caminho_arquivo, fileEncoding = "ISO-8859-1", sep = ";") 
 
-df$muni<-d$Município.de.residência
+# Visualizar as primeiras linhas do arquivo
+head(dados)
 
-#modificar nomes das variáveis
-names(d)<-c('muni','jan','fev','mar','abr')
+# Exibir os dados no Viewer do RStudio
+View(dados)
+
+# Separar colunas numéricas e de caracteres
+dados_numericos <- dados %>%
+  select(where(is.numeric))
+
+dados_caracteres <- dados %>%
+  select(where(~ is.character(.) | is.factor(.)))
+
+# Exibir as primeiras linhas dos dados numéricos e de caracteres
+head(dados_numericos)
+head(dados_caracteres)
+
+# Exibir as tabelas no Viewer do RStudio
+View(dados_numericos)
+View(dados_caracteres)
+
+# Carregar pacotes necessários
+library(dplyr)
+
+# Caminho do arquivo CSV
+caminho_arquivo <- 'input_data/sinannet_cnv_leptobr135749200_132_11_251_2013.csv'
+
+# Verificar se o arquivo existe antes de carregar
+if (file.exists(caminho_arquivo)) {
+  
+  # Tente carregar o arquivo com o delimitador correto (vírgula ou ponto e vírgula)
+  dados <- read.csv(caminho_arquivo, fileEncoding = "ISO-8859-1", sep = ";") 
+  
+  # Visualizar as primeiras linhas do arquivo
+  head(dados)
+  
+  # Exibir os dados no Viewer do RStudio
+  View(dados)
+  
+  # Separar colunas numéricas e de caracteres
+  dados_numericos <- dados %>%
+    select(where(is.numeric))
+  
+  dados_caracteres <- dados %>%
+    select(where(~ is.character(.) | is.factor(.)))
+  
+  # Exibir as primeiras linhas dos dados numéricos e de caracteres
+  head(dados_numericos)
+  head(dados_caracteres)
+  
+  # Exibir as tabelas no Viewer do RStudio
+  View(dados_numericos)
+  View(dados_caracteres)
+  
+} else {
+  message("Arquivo não encontrado: ", caminho_arquivo)
+}
+
