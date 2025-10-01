dbf3e8c66651aa5bf11cb910bb016e84f91a06e4
gandrat <gandratiagobr@gmail.comm>
2025-10-01 14:32:20 -0300

    Tentativa de resolver o mapa. Modificações no prepare_data. Ignorando pasta input_data.

--- 00_prepara_dados.R
+++ 00_prepara_dados.R
@@ -8,11 +8,10 @@ library(stringr)
 library(sf)
 library(purrr)
 library(janitor)
-library(here)
 
-# 1. Ler os dados de casos de todas as planilhas
+#1. Ler os dados de casos de todas as planilhas-------------
 # Certifique-se de que o seu diretório de trabalho está definido para a pasta PIE-Projeto
-arquivos <- list.files(here("inputdata_planilhas"), pattern = "^\\d{4}_(esquistossomose|leptospirose)\\.csv$", full.names = TRUE)
+arquivos <- list.files("input_data", pattern = ".csv", full.names = TRUE)
 
 ler_planilha_final <- function(arquivo) {
   # Extrai o ano e a doença do nome do arquivo
@@ -29,7 +28,7 @@ ler_planilha_final <- function(arquivo) {
     show_col_types = FALSE
   )
   
-  # A correção definitiva: renomear a primeira coluna pelo seu índice
+#A correção definitiva: renomear a primeira coluna pelo seu índice
   names(raw_data)[1] <- "municipio_notificacao"
   
   data_limpa <- raw_data %>%
@@ -57,30 +56,39 @@ ler_planilha_final <- function(arquivo) {
   return(data_limpa)
 }
 
-# Combina todas as planilhas em um único data frame
-dados_municipios <- purrr::map_dfr(arquivos, ler_planilha_final)
+## Combina todas as planilhas em um único data frame--------------
+casos_mun <- purrr::map_dfr(arquivos, ler_planilha_final)
 
-# 2. Ler os dados geográficos e de população
-mun_shape <- st_read(here("shapes/Municipios_BR.gpkg"), quiet = TRUE) %>%
-  st_transform(crs = "+proj=longlat +datum=WGS84")
+casos_mun<-casos_mun%>%mutate(mes_num=recode(mes,
+                                             "Jan" = 01, "Fev" = 02, "Mar" = 03, "Abr" = 4, "Mai" = 5, "Jun" = 6,
+                                             "Jul" = 7, "Ago" = 8, "Set" = 9, "Out" = 10, "Nov" = 11, "Dez" = 12),
+                              data=as.Date(paste0(ano,'-',mes_num,'-','01')))
+                              
+# 2. Ler os dados geográficos e de população--------------
+mun <- st_read("input_data/shapes/BR_Municipios_2024.shp", quiet = TRUE)
+mun$cod_mun<-substring(mun$CD_MUN, 1, 6)
 
-est_shape <- st_read(here("shapes/Estados_BR.gpkg"), quiet = TRUE) %>%
-  st_set_crs(st_crs(mun_shape)) %>%
-  st_transform(crs = "+proj=longlat +datum=WGS84")
+uf <- st_read("input_data/shapes/Estados_BR.gpkg", quiet = TRUE)
 
-# 3. Aggregar dados de casos para o nível de estado e fazer a junção com a população
-dados_estados <- dados_municipios %>%
+# 3. Aggregar dados de casos para o nível de estado e fazer a junção com a população----------
+casos_uf <- casos_mun %>%
   as_tibble() %>%
-  left_join(st_drop_geometry(mun_shape) %>% mutate(CD_MUN = str_sub(CD_MUN, 1, 6)), by = c("cod_mun" = "CD_MUN")) %>%
+  left_join(st_drop_geometry(mun) %>% mutate(CD_MUN = str_sub(CD_MUN, 1, 6)), by = c("cod_mun" = "CD_MUN")) %>%
   group_by(NM_UF, ano, doenca) %>% 
   summarise(
     casos_total = sum(casos, na.rm = TRUE),
     .groups = "drop"
   ) %>%
-  left_join(st_drop_geometry(est_shape), by = "NM_UF") %>%
+  left_join(st_drop_geometry(uf), by = "NM_UF") %>%
   mutate(
     casos_por_100k = (casos_total / POPULACAO) * 100000
   )
 
-# 4. Salvar os objetos em um único arquivo .rda
-save(dados_municipios, dados_estados, mun_shape, est_shape, file = here("data/doencas_dados.rda"))
+# dados dos municípios com a geometria do mapa
+# pensar numa outra forma de agregar geometrias à série temporal
+# mapa_dados_municipios <- left_join(mun, dados_municipios, by = "cod_mun")
+
+# 4. Salvar os objetos em um único arquivo .rda-------
+save(casos_mun, casos_uf, mun, uf, file = "output_data/doencas_dados.rda")
+
+plot(uf)
--- app_simples.R
+++ app_simples.R
@@ -16,14 +16,16 @@ library('leaflet.extras')
 # --- Carregar e Preparar os Dados ----
 # arquivo .rda que gerou
 load('output_data/doencas_dados.rda')
+mun<-st_transform(mun,crs='EPSG:4326')
+uf<-st_transform(uf,crs='EPSG:4326')
 
 basemap = leaflet(option=leafletOptions(zoomControl=FALSE)) %>% 
   addTiles() %>% 
   addLayersControl(
     position = "bottomright",
-    overlayGroups = c("Mapa de Densidade de Mortes","Municípios: Mortes por 100 mil","Municípios: Índice de Vulnerabilidade Social (IVS)","Estados: Casos por 100 mil","Estados: Mortes por 100 mil"),
+    overlayGroups = c("Mapa1"),
     options = layersControlOptions(collapsed = FALSE)) %>% 
-  hideGroup(c("Municípios: Mortes por 100 mil","Estados: Casos por 100 mil","Estados: Mortes por 100 mil","Municípios: Índice de Vulnerabilidade Social (IVS)"))  %>%
+  # hideGroup(c("Municípios: Mortes por 100 mil","Estados: Casos por 100 mil","Estados: Mortes por 100 mil","Municípios: Índice de Vulnerabilidade Social (IVS)"))  %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   setView(-50, -18, 4.4)
 
@@ -37,33 +39,38 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                  tabPanel("Mapa dos Casos",
                           div(class="outer",
                               leafletOutput("mymap", width="100%", height="100%"),
-
+                              
                               absolutePanel(id = "controls", class = "panel panel-default",
                                             top = 100, left = 10, width = 300, fixed=TRUE,
                                             draggable = TRUE, height = "auto",
-
+                                            
                                             h4("Controles"),
                                             p("Selecione as opções abaixo para filtrar os dados."),
-
+                                            
                                             selectInput("doenca_selecionada", "Selecione a Doença:",
                                                         choices = unique(casos_mun$doenca),
                                                         selected = unique(casos_mun$doenca)[1]), # Adicionado
-
+                                            
                                             selectInput("ano_selecionado", "Selecione o Ano:",
                                                         choices = unique(casos_mun$ano),
                                                         selected = unique(casos_mun$ano)[1]), # Adicionado
-
-                                            # Painel para exibir os totais
-                                            h3(textOutput("total_casos"), align = "right"),
-                                            h4(textOutput("num_municipios"), align = "right"),
-
-                                            # Gráfico simplificado
-                                            # plotOutput("grafico_casos_total", height="180px", width="100%")
-                              )
+                                            
+                                            selectInput("mes_selecionado", "Selecione o Mês:",
+                                                        choices = unique(casos_mun$mes),
+                                                        selected = unique(casos_mun$mes)[1]),
+                                            
+                                            actionButton("update_view", "Gerar Mapa!")),
+                              
+                              # Painel para exibir os totais
+                              h3(textOutput("total_casos"), align = "right"),
+                              h4(textOutput("num_municipios"), align = "right")
+                              
+                              # Gráfico simplificado
+                              # plotOutput("grafico_casos_total", height="180px", width="100%")
                           )
                  ),
                  
-
+                 
                  
                  ## Gráficos por Estados-----
                  tabPanel("Gráficos por Estados",
@@ -86,15 +93,10 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                  
 )
 
+plot(uf)
 # backend---------
 server <- function(input, output) {
-##Reactive DBs------------  
-  # Reactive para o mapa: Filtra os dados de acordo com a seleção do usuário
-  dados_mapa_filtrados <- reactive({
-    req(input$doenca_selecionada, input$ano_selecionado)
-    casos_mun %>%
-      filter(doenca == input$doenca_selecionada, ano == input$ano_selecionado)
-  })
+  ##Reactive DBs------------  
   
   # Reactive para os gráficos: Filtra dados de estados
   casos_uf_db <- reactive({
@@ -103,20 +105,40 @@ server <- function(input, output) {
       filter(doenca == input$doenca_estado, NM_UF %in% input$estado_select)
   })
   
+  casos_uf_db_sf <- reactive({
+    req(input$doenca_selecionada, input$ano_selecionado, input$mes_selecionado)
+    casos_uf %>%
+      filter(doenca == input$doenca_selecionada,
+             ano==input$ano_selecionado)%>%
+      left_join(uf,by='CD_UF')
+  })
+  
+  # Reactive para o mapa: Filtra os dados de acordo com a seleção do usuário
+  casos_mun_db <- reactive({
+    req(input$doenca_selecionada, input$ano_selecionado)
+    casos_mun %>%
+      filter(doenca == input$doenca_selecionada, 
+             ano == input$ano_selecionado,
+             mes==input$mes_selecionado)%>%
+      left_join(mun,by='cod_mun')
+  })
+  
   # Totais
   output$total_casos <- renderText({
-    casos_total <- sum(dados_mapa_filtrados()$casos, na.rm = TRUE)
+    casos_total <- sum(casos_mun_db()$casos, na.rm = TRUE)
     paste0(format(casos_total, big.mark=","), " casos")
   })
   
+  
+  
   output$num_municipios <- renderText({
-    n_mun <- dados_mapa_filtrados() %>% distinct(nome_mun) %>% nrow()
+    n_mun <- casos_mun_db() %>% distinct(nome_mun) %>% nrow()
     paste0(n_mun, " municípios afetados")
   })
   
   ## Gráficos--------------
   ###Casos por estados----------
-
+  
   # Gráfico por Estados
   output$grafico_estados <- renderPlotly({
     g <- ggplot(casos_uf_db(), aes(x = ano, y = casos_total, color = NM_UF, group = NM_UF)) +
@@ -129,36 +151,21 @@ server <- function(input, output) {
     ggplotly(g)
   })
   
-  # output$mymap <- renderLeaflet({ 
-  #   basemap})
-  ## Mapa Leaflet----------
-  # output$mymap <- renderLeaflet({
-  #   leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
-  #     addProviderTiles(providers$CartoDB.Positron) %>%
-  #     setView(-50, -18, 4.4)
+  output$mymap <- renderLeaflet({
+    basemap})
+  
+  
+  # Mapa Leaflet----------
+  
+  # observeEvent(input$update_view,{
+  #   leafletProxy("mymap") %>% 
+  #     # clearMarkers() %>%
+  #     
+  #     addPolygons(data = casos_uf_db_sf(), 
+  #                 color=c("#A50026"))
   # })
-  # 
-  # Atualize o mapa Leaflet com os dados filtrados
-  # observeEvent(input$doenca_selecionada, {
-  # 
-  #   pal <- colorBin("YlOrRd", domain = uf$POPULACAO)
-  # 
-    # leafletProxy("mymap",data=uf)%>% 
-    # 
-    # 
-    #   addPolygons(data=uf,
-    #     # fillColor = ~pal(AREA_KM2),
-    #     fillOpacity = 0.7,
-    #     color = "white",
-    #     weight = 1
-    #   )
-    # %>%
-  #   #     pal = pal,
-  #   #     values = ~casos,
-  #   #     title = "Casos",
-  #   #     position = "bottomright")
-  # })
+  
+  
 }
-
 # Execute o aplicativo
 shinyApp(ui = ui, server = server)
