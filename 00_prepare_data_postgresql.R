dd3c85fadecdb65b216d1d3235baf169c6af6383
gandrat <gandratiagobr@gmail.comm>
2025-10-09 17:08:53 -0300

    APP V2, super limpo, só com gráficos. Preparação de dados provenientes do postgres.

--- 00_prepare_data_postgresql.R
+++ 00_prepare_data_postgresql.R
@@ -1,5 +1,6 @@
 library(DBI)
 library(RPostgres)
+library(dplyr)
 
 con <- dbConnect(
   RPostgres::Postgres(), # Or RPostgreSQL::PostgreSQL()
@@ -7,7 +8,18 @@ con <- dbConnect(
   host = "200.132.11.22",
   port = 1305, # Or your specific port
   user = "guardia",
-  password = "guardia_2025"
+  password = "gda_2025"
 )
 
+load('output_data/doencas_dados.rda')
+
 ##Testando conexão
+# casos_mun<-dbGetQuery(con,"select * from caso");
+casos_uf<-dbGetQuery(con,"select * from casos_uf;")
+casos_uf<-casos_uf%>%mutate(ano=format(data,'%Y'),
+                            mes=format(data,'%m'))
+
+uf<-dbGetQuery(con,"select * from uf;")
+
+
+save(casos_uf, uf, file='output_data/doencas_dados_v2.rda')
