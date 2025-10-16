cbf70ddfd7681a6648b30eefe0bf83c0fd0a8ddc
isabellicruz <isabellicosta2013@gmail.com>
2025-10-16 17:31:52 -0300

    implementação dos gráficos de regiões imediatas

--- 00_prepare_data_postgresql.R
+++ 00_prepare_data_postgresql.R
@@ -1,57 +1,9 @@
-library(DBI)
-library(RPostgres)
-library(dplyr)
-
-# Detalhes de Conexão (Assumindo que estão corretos)
-con <- dbConnect(
-  RPostgres::Postgres(),
-  dbname = "guardiaguas",
-  host = "200.132.11.22",
-  port = 1305,
-  user = "guardia",
-  password = "gda_2025"
-)
-
-# 1. Dados de Casos por UF (Estados)
-# Tabela original no banco: casos_uf
-casos_uf<-dbGetQuery(con,"select * from casos_uf;")
-
-# Adiciona colunas 'ano' e 'mes'
-casos_uf<-casos_uf %>%
-  mutate(
-    ano = format(data,'%Y'),
-    mes = format(data,'%m')
-  )
-
-# 2. DADOS DE CASOS POR REGIÃO IMEDIATA (RGI)
-# Tabela original no banco: casos_regiao
-# As colunas nm_rgi e nm_rgint JÁ EXISTEM, conforme a imagem, não precisa renomear.
-casos_rgi<-dbGetQuery(con,"select * from casos_rgi;")
-
-casos_rgi<-casos_rgi %>%
-  mutate(
-    # Adiciona colunas 'ano' e 'mes'
-    ano = format(data, '%Y'),
-    mes = format(data, '%m'),
-    # Calcula a Prevalência (prev) para RGI: (total_casos / pop_total) * 100000
-    prev = (total_casos / pop_total) * 100000
-  )
-
-# 3. Dados Auxiliares (UF)
-uf<-dbGetQuery(con,"select * from uf;")
-
-save(casos_uf, casos_rgi, uf, file='output_data/doencas_dados_v3.rda') 
-
-print("Arquivos de dados (doencas_dados_v3.rda) criados e salvos com sucesso!")
-
-
 # script_pre_processamento.R
 
 library(DBI)
 library(RPostgres)
 library(dplyr)
 
-# Detalhes de Conexão (AJUSTE CONFORME NECESSÁRIO)
 con <- dbConnect(
   RPostgres::Postgres(),
   dbname = "guardiaguas",
@@ -64,33 +16,27 @@ con <- dbConnect(
 # 1. Dados de Casos por UF (Estados)
 # Tabela original no banco: casos_uf
 casos_uf <- dbGetQuery(con,"select * from casos_uf;")
-
-# Adiciona colunas 'ano' e 'mes' e calcula a taxa de prevalência (se faltar)
 casos_uf <- casos_uf %>%
   mutate(
     ano = format(data,'%Y'),
     mes = format(data,'%m'),
-    # Calcula a Prevalência (prev) para UF: (total_casos / pop_total) * 100000
+    # Calcula a Prevalência 
     prev = (total_casos / pop_total) * 100000 
   )
 
-# Executa a consulta
 casos_rgi <- dbGetQuery(con,"select * from casos_rgi;")
-
 casos_rgi <- casos_rgi %>%
   mutate(
     # Adiciona colunas 'ano' e 'mes'
     ano = format(data, '%Y'),
     mes = format(data, '%m'),
-    # Calcula a Prevalência (prev) para RGI: (total_casos / pop_total) * 100000
+    # Calcula a Prevalência
     prev = (total_casos / pop_total) * 100000
   )
 
-# 3. Dados Auxiliares (UF)
 uf <- dbGetQuery(con,"select * from uf;")
 
-# Salva TODOS os data frames necessários no arquivo .rda
-# Garanta que a pasta 'output_data' exista!
-save(casos_uf, casos_rgi, uf, file='output_data/doencas_dados_v3.rda') 
 
-print("Arquivos de dados (doencas_dados_v3.rda) criados e salvos com sucesso!")
+save(casos_uf, casos_rgi, uf, file='output_data/doencas_dados_v2.rda') 
+
+print("Arquivos de dados (doencas_dados_v2.rda) criados e salvos com sucesso!")
--- app_v2.R
+++ app_v2.R
@@ -25,15 +25,30 @@ casos_uf <- casos_uf %>%
       mes %in% c("09", "10", "11") ~ "Primavera",        
       TRUE ~ "Não Classificado" 
     ),
-    ano = lubridate::year(data) # Extrai o ano da coluna 'data'
+    ano = lubridate::year(data)
   )
 
-# --- Preparação de dados para o pickerInput ---
+imediatas_regiao <- casos_rgi %>%
+  select(nm_rgi, nm_rgint, nm_regia) %>%
+  distinct() %>%
+  arrange(nm_regia, nm_rgint, nm_rgi)
+
+intermed_por_regiao <- imediatas_regiao %>%
+  select(nm_rgint, nm_regia) %>%
+  distinct() %>%
+  arrange(nm_regia, nm_rgint)
+
+intermed_por_regiao <- split(intermed_por_regiao$nm_rgint, intermed_por_regiao$nm_regia)
+
+imediatas_por_intermed <- imediatas_regiao %>%
+  select(nm_rgi, nm_rgint) %>%
+  distinct() %>%
+  arrange(nm_rgint, nm_rgi)
+
 estados_regiao <- casos_uf %>%
   select(nm_uf, nm_regia) %>%
   distinct() %>%
   arrange(nm_regia, nm_uf)
-
 estados_por_regiao <- split(estados_regiao$nm_uf, estados_regiao$nm_regia)
 
 ##Basemap----------------
@@ -41,7 +56,7 @@ basemap = leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
   addTiles() %>% 
   addLayersControl(
     position = "bottomright",
-    overlayGroups = c("Casos por UF"), # Nome do layer de visualização
+    overlayGroups = c("Casos por UF"),
     options = layersControlOptions(collapsed = FALSE)) %>% 
   addProviderTiles(providers$CartoDB.Positron) %>%
   setView(-50, -18, 4.4)
@@ -52,9 +67,7 @@ basemap
 ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                  title="Monitoramento de Doenças", id="nav",
                  
-                 
                  ## Aba Gráficos por Estados-----
-                 
                  tabPanel("Gráficos por Estados",
                           sidebarLayout(
                             sidebarPanel(
@@ -66,10 +79,7 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                                           options = list(`actions-box` = TRUE),
                                           selected = unique(casos_uf$nm_uf)[1:5],
                                           multiple = TRUE),
-                              
                               hr(),
-                              
-                        
                               pickerInput("estacao_select", "Estações:",
                                           choices = unique(casos_uf$estacao)[unique(casos_uf$estacao) != "Não Classificado"],
                                           selected = "Verão",
@@ -86,21 +96,76 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                               plotlyOutput("grafico_estacao")
                             )
                           )
+                 ), 
+                 
+                 ## Aba Gráficos por Regiões Imediatas-----
+                 tabPanel("Gráficos por Regiões Imediatas",
+                          sidebarLayout(
+                            sidebarPanel(
+                              pickerInput("doenca_rgi", "Selecione a Doença:",
+                                          choices = unique(casos_rgi$doenca),
+                                          selected = "Leptospirose"),
+                              pickerInput("rgint_select", "Por Região Intermediária:",
+                                          choices = intermed_por_regiao,
+                                          options = list(`actions-box` = TRUE),
+                                          selected = unique(casos_rgi$nm_rgint)[1],
+                                          multiple = FALSE),
+                              pickerInput("rgi_select", "Por Região Imediata:",
+                                          choices = NULL,
+                                          options = list(`actions-box` = TRUE),
+                                          multiple = TRUE),
+                              hr(),
+                              pickerInput("estacao_select_rgi", "Estações:",
+                                          choices = unique(casos_rgi$estacao)[unique(casos_rgi$estacao) != "Não Classificado"],
+                                          selected = "Verão",
+                                          multiple = FALSE)
+                            ),
+                            mainPanel(
+                              h4("Casos Totais por Data"),
+                              plotlyOutput("grafico_imediatas"),
+                              hr(),
+                              h4("Casos por 100 mil Habitantes"),
+                              plotlyOutput("grafico_taxa_imediatas"),
+                              hr(),
+                              h4("Casos por Estações dos anos"),
+                              plotlyOutput("grafico_estacao_imediatas")
+                            )
+                          )
                  )
 )
 
 # backend---------
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  observeEvent(input$rgint_select, {
+    req(input$rgint_select)
+    regioes_filtradas <- imediatas_por_intermed %>%
+      filter(nm_rgint == input$rgint_select) %>%
+      pull(nm_rgi)
+    updatePickerInput(
+      session,
+      "rgi_select",
+      choices = regioes_filtradas,
+      selected = head(regioes_filtradas, 5)
+    )
+  })
   
   ##Gráficos de Série Temporal------------	
   
+  # ----------- ESTADOS ------------------
   casos_uf_db <- reactive({
     req(input$doenca_estado, input$estado_select)
     casos_uf %>%	
       filter(doenca == input$doenca_estado, nm_uf %in% input$estado_select)	
   })
   
-  #Gráfico de Estações
+  # ----------- REGIOES IMEDIATAS --------
+  casos_rgi_db <- reactive({
+    req(input$doenca_rgi, input$rgint_select, input$rgi_select)
+    casos_rgi %>%	
+      filter(doenca == input$doenca_rgi, nm_rgint == input$rgint_select, nm_rgi %in% input$rgi_select)
+  })
+  
   casos_uf_verao_db <- reactive({
     req(input$doenca_estado, input$estado_select, input$estacao_select)
     casos_uf %>%
@@ -113,56 +178,76 @@ server <- function(input, output) {
       summarise(total_casos_estacao = sum(total_casos, na.rm = TRUE), .groups = 'drop')
   })
   
-  ## Gráficos--------------
+  casos_rgi_verao_db <- reactive({
+    req(input$doenca_rgi, input$rgint_select, input$rgi_select, input$estacao_select_rgi)
+    casos_rgi %>%
+      filter(
+        doenca == input$doenca_rgi, 
+        nm_rgint == input$rgint_select,
+        nm_rgi %in% input$rgi_select,
+        estacao == input$estacao_select_rgi
+      ) %>%
+      group_by(nm_rgi, ano) %>%
+      summarise(total_casos_estacao = sum(total_casos, na.rm = TRUE), .groups = 'drop')
+  })
   
-  # 1. Gráfico de Casos Totais por Data (Série Temporal)
   output$grafico_estados <- renderPlotly({
     g <- ggplot(casos_uf_db(), aes(x = data, y = total_casos, color = nm_uf, group = nm_uf)) +
       geom_line() +
-      labs(title = "",	
-           x = "Data", y = "Nº de Casos Totais",
-           color = "Estado") +
+      labs(title = "", x = "Data", y = "Nº de Casos Totais", color = "Estado") +
       theme_minimal() +
       theme(legend.title = element_text(size = 12)) 
-    
     ggplotly(g)
   })
   
-  # 2. Gráfico de Casos por 100 mil (Taxa) por Data
+  output$grafico_imediatas <- renderPlotly({
+    g <- ggplot(casos_rgi_db(), aes(x = data, y = total_casos, color = nm_rgi, group = nm_rgi)) +
+      geom_line() +
+      labs(title = "", x = "Data", y = "Nº de Casos Totais", color = "Região Imediata") +
+      theme_minimal() +
+      theme(legend.title = element_text(size = 12)) 
+    ggplotly(g)
+  })
+  
   output$grafico_taxa_estados <- renderPlotly({
     g <- ggplot(casos_uf_db(), aes(x = data, y = prev, color = nm_uf, group = nm_uf)) +
       geom_line() +
-      labs(title = "",	
-           x = "Data", y = "Casos por 100 mil hab.",
-           color = "Estado") +
+      labs(title = "", x = "Data", y = "Casos por 100 mil hab.", color = "Estado") +
       theme_minimal() +
       theme(legend.title = element_text(size = 12)) 
-    
     ggplotly(g)
   })
   
-  # Gráfico para Análise Sazonal
+  output$grafico_taxa_imediatas <- renderPlotly({
+    g <- ggplot(casos_rgi_db(), aes(x = data, y = prev, color = nm_rgi, group = nm_rgi)) +
+      geom_line() +
+      labs(title = "", x = "Data", y = "Casos por 100 mil hab.", color = "Região Imediata") +
+      theme_minimal() +
+      theme(legend.title = element_text(size = 12)) 
+    ggplotly(g)
+  })
+  
   output$grafico_estacao <- renderPlotly({
     data_plot <- casos_uf_verao_db()
-    
     g <- ggplot(data_plot, aes(x = ano, y = total_casos_estacao, fill = nm_uf, group = nm_uf, text = paste("Ano:", ano, "<br>Casos:", total_casos_estacao))) +
       geom_bar(stat = "identity", position = "dodge") +
-      labs(title = "",
-           x = "Ano",
-           y = paste("Casos por estações", unique(data_plot$estacao)),
-           fill = "Estado") +
+      labs(title = "", x = "Ano", y = paste("Casos por estações", unique(data_plot$estacao)), fill = "Estado") +
       scale_x_continuous(breaks = unique(data_plot$ano)) + 
       theme_minimal() +
       theme(legend.title = element_text(size = 12))
-    
     ggplotly(g, tooltip = "text") 
   })
   
-  # Mapa Base
-  output$mymap <- renderLeaflet({
-    basemap
+  output$grafico_estacao_imediatas <- renderPlotly({
+    data_plot <- casos_rgi_verao_db()
+    g <- ggplot(data_plot, aes(x = ano, y = total_casos_estacao, fill = nm_rgi, group = nm_rgi, text = paste("Ano:", ano, "<br>Casos:", total_casos_estacao))) +
+      geom_bar(stat = "identity", position = "dodge") +
+      labs(title = "", x = "Ano", y = paste("Casos por estações", unique(data_plot$estacao)), fill = "Região Imediata") +
+      scale_x_continuous(breaks = unique(data_plot$ano)) + 
+      theme_minimal() +
+      theme(legend.title = element_text(size = 12))
+    ggplotly(g, tooltip = "text") 
   })
-  
 }
-# Execute o aplicativo
+
 shinyApp(ui = ui, server = server)
