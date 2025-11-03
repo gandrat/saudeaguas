35a9fbf67c57f749225f8af2abb765b802543659
gandrat <gandratiagobr@gmail.comm>
2025-11-03 15:14:25 -0300

    Separação dos estados nos municipios; filtro dos municipios adicionados e separados conforme a ordem que os estados forem selecionados; erro do gráfico das estações resolvido; mapa de prevalência com o nome ajustado; filtros funcionando corretamente e mapa de prevalência com os filtros de datas também funcionando

--- 00_prepare_data_postgresql.R
+++ 00_prepare_data_postgresql.R
@@ -1,11 +1,11 @@
-# script_pre_processamento.R 
+# script_pre_processamento
 
 library(DBI)
 library(RPostgres)
 library(dplyr)
 library(lubridate)
 
-# --- ESTAÇÕES ---
+# ESTAÇÕES -----
 define_estacao <- function(df) {
   df %>%
     mutate(
@@ -24,7 +24,7 @@ define_estacao <- function(df) {
     )
 }
 
-# --- CONEXÃO ---
+# CONEXÃO ----
 
 # Detalhes de Conexão
 con <- dbConnect(
@@ -36,81 +36,101 @@ con <- dbConnect(
   password = "gda_2025"
 )
 
-# 1. Casos por UF (Estados)
-casos_uf <- dbGetQuery(con,"select * from casos_uf;")
+# 1. CASOS MUNICIPIOS
+casos <- " SELECT t1.geo_cod, t1.nm_mun, t1.cd_rgi, t1.nm_rgi, t1.cd_rgint, t1.nm_rgint,
+t1.cd_uf, t1.nm_uf, t1.cd_regia, t1.nm_regia, t1.id_doenca, t1.doenca,
+t1.data, t1.mes, t1.total_casos, t1.pop_total, t1.prev,t2.lat, t2.lon
+FROM casos_municipios_mes AS t1
 
-# Adiciona colunas 'ano', 'mes' e calcula a taxa de prevalência
-casos_uf <- casos_uf %>%
-  mutate(
-    ano = format(data,'%Y'),
-    mes = format(data,'%m'),
-    prev = (total_casos / pop_total) * 100000 
-  )
-casos_uf <- define_estacao(casos_uf)
+LEFT JOIN regic_2018 AS t2
+ON t1.geo_cod = t2.geo_cod;
+"
 
+casos_municipios_mes <- dbGetQuery(con, casos)
 
-# 2. REGIÃO IMEDIATA (RGI)
-casos_rgi <- dbGetQuery(con,"select * from casos_rgi;")
-
-casos_rgi <- casos_rgi %>%
+casos_municipios_mes <- casos_municipios_mes %>%
   mutate(
     ano = format(data, '%Y'),
-    mes = format(data, '%m'),
+    mes = format(data, '%m')
+  )
+casos_municipios_mes <- define_estacao(casos_municipios_mes)
+
+
+# 2. TABELAS -----
+
+# Casos por UF
+casos_uf <- casos_municipios_mes %>%
+  group_by(nm_uf, nm_regia, doenca, data, estacao, ano) %>% 
+  summarise(
+    total_casos = sum(total_casos, na.rm = TRUE),
+    pop_total = sum(pop_total, na.rm = TRUE),
+    .groups = 'drop'
+  ) %>%
+  mutate(
     prev = (total_casos / pop_total) * 100000
   )
-casos_rgi <- define_estacao(casos_rgi)
 
-# 3. (UF)
-uf <- dbGetQuery(con,"select * from uf;")
+# Casos por Região Imediata
+casos_rgi <- casos_municipios_mes %>%
+  group_by(nm_rgi, nm_rgint, doenca, data, estacao, ano) %>% 
+  summarise(
+    total_casos = sum(total_casos, na.rm = TRUE),
+    pop_total = sum(pop_total, na.rm = TRUE),
+    .groups = 'drop'
+  ) %>%
+  mutate(
+    prev = (total_casos / pop_total) * 100000
+  )
 
-# 4. REGIC
+# 3. LOOKUPS -----
 
-regic<-dbGetQuery(con,"select * from casos_regic;")
-
-# --- LOOKUP ---
-
-# 1.  filtros de Regiões Imediatas
-imediatas_regiao <- casos_rgi %>%
+# filtros de Regiões Imediatas
+imediatas_regiao <- casos_municipios_mes %>%
   select(nm_rgi, nm_rgint, nm_regia) %>%
   distinct() %>%
   arrange(nm_regia, nm_rgint, nm_rgi)
 
-# RG. Intermediárias agrupadas por Região Geográfica (Aba 1)
+# Aba 1- RG. Intermediárias agrupadas por Região Geográfica
 intermed_por_regiao <- imediatas_regiao %>%
   select(nm_rgint, nm_regia) %>%
   distinct() %>%
   arrange(nm_regia, nm_rgint) %>%
-  split(.$nm_rgint, .$nm_regia) 
+  split(.$nm_rgint, .$nm_regia)
 
-#  filtro de Regiões Imediatas
+# filtro de Regiões Imediatas
 imediatas_por_intermed <- imediatas_regiao %>%
   select(nm_rgi, nm_rgint) %>%
   distinct() %>%
   arrange(nm_rgint, nm_rgi)
 
-# 2. filtros de Estados (Aba 1)
+# filtros de Estados (Aba 1)
 estados_regiao <- casos_uf %>%
   select(nm_uf, nm_regia) %>%
   distinct() %>%
   arrange(nm_regia, nm_uf)
 estados_por_regiao <- split(estados_regiao$nm_uf, estados_regiao$nm_regia)
 
-# 3. filtros de RGI/UF (Aba 2)
-rgi_estados_regiao <- casos_rgi %>%
+# filtros de RGI/UF (Aba 2)
+rgi_estados_regiao <- casos_municipios_mes %>%
   select(nm_uf, nm_regia) %>%
   distinct() %>%
   arrange(nm_regia, nm_uf)
 rgi_estados_por_regiao <- split(rgi_estados_regiao$nm_uf, rgi_estados_regiao$nm_regia)
 
-# Região Intermediária por Estado 
-intermed_por_estado_df <- casos_rgi %>%
+# Região Intermediária por Estado
+intermed_por_estado_df <- casos_municipios_mes %>%
   select(nm_uf, nm_rgint) %>%
   distinct() %>%
   arrange(nm_uf, nm_rgint)
 
-save(casos_uf, casos_rgi, uf, imediatas_regiao, intermed_por_regiao, imediatas_por_intermed,
-     estados_regiao, estados_por_regiao, rgi_estados_regiao, rgi_estados_por_regiao, regic,
+save(casos_uf, casos_rgi, casos_municipios_mes, imediatas_regiao, intermed_por_regiao, imediatas_por_intermed,
+     estados_regiao, estados_por_regiao, rgi_estados_regiao, rgi_estados_por_regiao,
      intermed_por_estado_df,
      file = 'output_data/doencas_dados_v2.rda')
+
+save(casos_municipios_mes, imediatas_regiao, intermed_por_regiao, imediatas_por_intermed,
+     estados_regiao, estados_por_regiao, rgi_estados_regiao, rgi_estados_por_regiao,
+     intermed_por_estado_df,
+     file = 'output_data/doencas_dados_v3.rda')
 
 print("Arquivos de dados (doencas_dados_v2.rda) criados e salvos com sucesso!")
--- app_v3.R
+++ app_v3.R
@@ -14,10 +14,10 @@ library('lubridate')
 library('RColorBrewer')
 
 #1.Carregue o arquivo rda!!!-----
-load('output_data/doencas_dados_v2.rda')
+load('output_data/doencas_dados_v3.rda')
 
-if (!inherits(regic$data, "Date")) {
-  regic$data <- as.Date(regic$data)
+if (!inherits(casos_municipios_mes$data, "Date")) {
+  casos_municipios_mes$data <- as.Date(casos_municipios_mes$data)
 }
 
 #ordem das estações-----
@@ -27,7 +27,7 @@ estacoes_ordem <- factor(
 )
 
 #datas para o slider--------
-datas_unicas_slider <- regic %>%
+datas_unicas_slider <- casos_municipios_mes %>%
   mutate(data_mes = floor_date(data, "month")) %>%
   pull(data_mes) %>%
   unique() %>%
@@ -59,7 +59,7 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                                         
                                         # Filtro de seleção de doenças - Slider-------
                                         pickerInput("map_doenca", "Selecione a Doença:",
-                                                    choices = unique(regic$doenca),
+                                                    choices = unique(casos_municipios_mes$doenca),
                                                     selected = "Leptospirose",
                                                     multiple = FALSE),
                                         
@@ -74,63 +74,47 @@ ui <- navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                           )
                  ), # <--- Fechamento correto da aba 1
                  
-                 ## 2. Aba Gráficos por Estados-----
-                 tabPanel("Gráficos por Estados",
-                          sidebarLayout(
-                            sidebarPanel(
-                              pickerInput("doenca_estado", "Selecione a Doença:",
-                                          choices = unique(casos_uf$doenca),
-                                          selected = "Leptospirose"),
-                              pickerInput("estado_select", "Estados por Região:",
-                                          choices = estados_por_regiao,
-                                          options = list(`actions-box` = TRUE),
-                                          selected = unique(casos_uf$nm_uf)[1:5],
-                                          multiple = TRUE)
-                            ),
-                            mainPanel(
-                              h4("Prevalência por Data"),
-                              plotlyOutput("grafico_estados"),
-                              hr(),
-                              h4("Prevalência por 100 mil Habitantes"),
-                              plotlyOutput("grafico_taxa_estados"),
-                              hr(),
-                              h4("Prevalência por Estações (Anos)"),
-                              plotlyOutput("grafico_estacao")
-                            )
-                          )
-                 ), # <--- Fechamento correto da aba 2
+                 
                  
                  ## 3. Aba Gráficos por Regiões Imediatas-----
-                 tabPanel("Gráficos por Regiões",
+                 tabPanel("Série Temporal",
                           sidebarLayout(
                             sidebarPanel(
                               pickerInput("doenca_rgi", "Selecione a Doença:",
-                                          choices = unique(casos_rgi$doenca),
+                                          choices = unique(casos_municipios_mes$doenca),
                                           selected = "Leptospirose"),
                               pickerInput("estado_select_rgi", "Estados:",
                                           choices = rgi_estados_por_regiao,
                                           options = list(`actions-box` = TRUE),
-                                          selected = unique(casos_rgi$nm_uf)[1:3],
+                                          selected = unique(casos_municipios_mes$nm_uf),
                                           multiple = TRUE),
                               pickerInput("rgint_select", "Por Região Intermediária:",
                                           choices = NULL,
                                           options = list(`actions-box` = TRUE),
+                                          selected = unique(casos_municipios_mes$nm_rgint),
                                           multiple = TRUE),
                               pickerInput("rgi_select", "Por Região Imediata:",
                                           choices = NULL,
                                           options = list(`actions-box` = TRUE),
+                                          selected = unique(casos_municipios_mes$nm_rgi),
+                                          multiple = TRUE),
+                              pickerInput("mun_select", "Por Município:",
+                                          choices = NULL,
+                                          options = list(`actions-box` = TRUE),
+                                          selected = unique(casos_municipios_mes$nm_mun),
                                           multiple = TRUE)
                             ),
                             mainPanel(
-                              h4("Prevalência por Data"),
-                              plotlyOutput("grafico_imediatas"),
+                              h4("Casos"),
+                              plotlyOutput("grafico_mun"),
                               hr(),
-                              h4("Prevalência por 100 mil Habitantes"),
-                              plotlyOutput("grafico_taxa_imediatas"),
+                              h4("Prevalência"),
+                              plotlyOutput("grafico_taxa_mun"),
                               hr(),
-                              h4("Prevalência por Estações (Anos)"),
-                              plotlyOutput("grafico_estacao_imediatas")
-                            )
+                              h4("Gráficos por Estação"),
+                              plotlyOutput("grafico_estacao"),
+                              hr())
+                            
                           )
                  ), # <--- Fechamento correto da aba 3
                  
@@ -253,48 +237,32 @@ server <- function(input, output, session) {
   
   ##Gráficos de Série Temporal.-----
   
-  ####ESTADOS----------
-  casos_uf_db <- reactive({
-    req(input$doenca_estado, input$estado_select)
-    casos_uf %>%
-      filter(doenca == input$doenca_estado, nm_uf %in% input$estado_select)
+  
+  ##BDs reativo----------------
+  ### Gráficos--------
+  casos_mun_db <- reactive({
+    req(input$doenca_rgi, input$rgint_select, input$rgi_select,input$rgi_select )
+    casos_municipios_mes %>%group_by(data,estacao)%>%
+      filter(doenca == input$doenca_rgi, 
+             nm_rgint %in% input$rgint_select, 
+             nm_rgi %in% input$rgi_select)%>%
+      summarise(total_casos=sum(total_casos,na.rm=T),
+                prev=mean(prev,na.rm=T))
   })
   
-  ####REGIOES IMEDIATAS-----------
-  casos_rgi_db <- reactive({
-    req(input$doenca_rgi, input$rgint_select, input$rgi_select)
-    casos_rgi %>%
-      filter(doenca == input$doenca_rgi, nm_rgint %in% input$rgint_select, nm_rgi %in% input$rgi_select)
-  })
+  ### Mapas-----------
+  observeEvent(c(input$map_date, input$map_doenca), {
+    req(input$map_date, input$map_doenca)
+    
   
-  ####ESTAÇÕES ESTADOS---------------
-  casos_uf_estacao_db <- reactive({
-    req(input$doenca_estado, input$estado_select)
-    casos_uf %>%
-      filter(
-        doenca == input$doenca_estado,
-        nm_uf %in% input$estado_select,
-        estacao != "Não Classificado"
-      ) %>%
-      group_by(nm_uf, doenca, estacao) %>%
-      summarise(total_casos_estacao = sum(total_casos, na.rm = TRUE), .groups = 'drop') %>%
-      mutate(estacao = factor(estacao, levels = levels(estacoes_ordem)))
-  })
+    data_filtrada <- floor_date(input$map_date, "month")
+    
+    casos_mun_db_map <- casos_municipios_mes %>%
+      filter(data == data_filtrada,
+             doenca == input$map_doenca,
+             prev > 0)
   
-  ####ESTAÇÕES RGI--------------
-  casos_rgi_estacao_db <- reactive({
-    req(input$doenca_rgi, input$rgint_select, input$rgi_select)
-    casos_rgi %>%
-      filter(
-        doenca == input$doenca_rgi,
-        nm_rgint %in% input$rgint_select,
-        nm_rgi %in% input$rgi_select,
-        estacao != "Não Classificado"
-      ) %>%
-      group_by(nm_rgi, doenca, estacao) %>%
-      summarise(total_casos_estacao = sum(total_casos, na.rm = TRUE), .groups = 'drop') %>%
-      mutate(estacao = factor(estacao, levels = levels(estacoes_ordem)))
-  })
+  
   
   
   output$mymap <- renderLeaflet({
@@ -302,36 +270,19 @@ server <- function(input, output, session) {
   })
   
   #GRÁFICOS---------
-  
-  output$grafico_estados <- renderPlotly({
-    g <- ggplot(casos_uf_db(), aes(x = data, y = total_casos, fill = nm_uf, group = nm_uf)) +
+  ##Gráfico de casos por data----------
+  output$grafico_mun <- renderPlotly({
+    g <- ggplot(casos_mun_db(), aes(x = data, y = total_casos)) +
       geom_col(position = "stack") +
-      labs(title = "", x = "Data", y = "Casos", fill = "Estado") +
+      labs(title = "", x = "Data", y = "Nº de Casos", fill = "Região Imediata") +
       theme_minimal() +
       theme(legend.title = element_text(size = 12))
     ggplotly(g)
   })
   
-  output$grafico_imediatas <- renderPlotly({
-    g <- ggplot(casos_rgi_db(), aes(x = data, y = total_casos, fill = nm_rgi, group = nm_rgi)) +
-      geom_col(position = "stack") +
-      labs(title = "", x = "Data", y = "de Casos Totais", fill = "Região Imediata") +
-      theme_minimal() +
-      theme(legend.title = element_text(size = 12))
-    ggplotly(g)
-  })
-  
-  output$grafico_taxa_estados <- renderPlotly({
-    g <- ggplot(casos_uf_db(), aes(x = data, y = prev, fill = nm_uf, group = nm_uf)) +
-      geom_col(position = "stack") +
-      labs(title = "", x = "Data", y = "Casos por 100 mil hab.", fill = "Estado") +
-      theme_minimal() +
-      theme(legend.title = element_text(size = 12))
-    ggplotly(g)
-  })
-  
-  output$grafico_taxa_imediatas <- renderPlotly({
-    g <- ggplot(casos_rgi_db(), aes(x = data, y = prev, fill = nm_rgi, group = nm_rgi)) +
+  ##Gráfico de prevalencia por data--------------
+  output$grafico_taxa_mun <- renderPlotly({
+    g <- ggplot(casos_mun_db(), aes(x = data, y = prev)) +
       geom_col(position = "stack") +
       labs(title = "", x = "Data", y = "Casos por 100 mil hab.", fill = "Região Imediata") +
       theme_minimal() +
@@ -339,33 +290,16 @@ server <- function(input, output, session) {
     ggplotly(g)
   })
   
-  #GRÁFICO DE ESTAÇÃO - ESTADOS----------
-  output$grafico_estacao <- renderPlotly({
-    data_plot <- casos_uf_estacao_db()
-    
-    g <- ggplot(data_plot, aes(x = estacao, y = total_casos_estacao, fill = estacao,
-                               group = estacao,
-                               text = paste("Estado:", nm_uf, "<br>Estação:", estacao, "<br>Casos:", total_casos_estacao))) +
-      geom_col(position = "dodge") +
-      labs(title = "", x = "Estação", y = "Prevalência por Estações (Anos)", fill = "Estação") +
-      facet_wrap(~nm_uf, scales = "free_y") +
-      theme_minimal() +
-      theme(legend.title = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1))
-    
-    ggplotly(g, tooltip = "text") %>%
-      layout(hovermode = "x unified")
-  })
   
-  #GRÁFICO DE ESTAÇÃO - RGI------
-  output$grafico_estacao_imediatas <- renderPlotly({
-    data_plot <- casos_rgi_estacao_db()
+  
+  ##Gráfico por estações------
+  output$grafico_estacao <- renderPlotly({
+    data_plot <- casos_mun_db()
     
-    g <- ggplot(data_plot, aes(x = estacao, y = total_casos_estacao, fill = estacao,
-                               group = estacao,
-                               text = paste("Região:", nm_rgi, "<br>Estação:", estacao, "<br>Casos Totais:", total_casos_estacao))) +
+    g <- ggplot(data_plot, aes(x = estacao, y = total_casos, fill = estacao,
+                               group = estacao)) +
       geom_col(position = "dodge") +
       labs(title = "", x = "Estação", y = "Casos Totais (Anos)", fill = "Estação") +
-      facet_wrap(~nm_rgi, scales = "free_y") +
       theme_minimal() +
       theme(legend.title = element_text(size = 12), axis.text.x = element_text(angle = 45, hjust = 1))
     
@@ -374,16 +308,7 @@ server <- function(input, output, session) {
   })
   
   # MAPA----------------
-  observeEvent(c(input$map_date, input$map_doenca), {
-    req(input$map_date, input$map_doenca)
-    
-    #1.FILTRAGEM----------
-    data_filtrada <- floor_date(input$map_date, "month")
-    
-    dados_filtrados_regic <- regic %>%
-      filter(data == data_filtrada,
-             doenca == input$map_doenca,
-             prev > 0)
+
     
     leafletProxy("mymap", session) %>%
       clearMarkers() %>%
@@ -392,7 +317,7 @@ server <- function(input, output, session) {
       removeControl(layerId = "kernel_legend")
     
     # 2. ALERTA--------
-    if (nrow(dados_filtrados_regic) == 0) {
+    if (nrow(casos_mun_db_map) == 0) {
       showNotification(
         paste("Neste mês (", format(data_filtrada, "%m/%Y"), ") não houve nenhuma ocorrência desta doença (", input$map_doenca, ") no Brasil."),
         type = "warning",
@@ -402,11 +327,11 @@ server <- function(input, output, session) {
     }
     
     #3.FOCO, GRADIENTE E LEGENDA----------
-    max_prev <- max(dados_filtrados_regic$prev, na.rm = TRUE)
-    min_prev <- min(dados_filtrados_regic$prev, na.rm = TRUE)
+    max_prev <- max(casos_mun_db_map$prev, na.rm = TRUE)
+    min_prev <- min(casos_mun_db_map$prev, na.rm = TRUE)
     
     #intensidade do Kernel------
-   
+    
     kernel_colors <- rev(brewer.pal(n = 9, name = "Spectral"))
     paleta_legenda <- colorNumeric(
       palette = kernel_colors,
@@ -415,13 +340,15 @@ server <- function(input, output, session) {
     )
     
     leafletProxy("mymap", session) %>%
-      addCircleMarkers(data=dados_filtrados_regic, lat=~lat, lng=~lon, radius = ~(prev-min(prev))/(max(prev)-min(prev))*20,
+      addCircleMarkers(data=casos_mun_db_map, lat=~lat, lng=~lon, radius = ~(prev-min(prev))/(max(prev)-min(prev))*20,
                        group = "Marcadores de Prevalência",
-                       popup = ~paste0("Município: ", nome_cidad, "<br>Data: ", format(data, "%d/%m/%Y"), "<br>Doença: ", doenca,
+                       popup = ~paste0("Município: ", nm_mun, "<br>Data: ", format(data, "%d/%m/%Y"), "<br>Doença: ", doenca,
                                        "<br>Prevalência (100k): ", round(prev, 2))) %>%
       
       #Mapa de Calor (Kernel)---------
-    addHeatmap(data = dados_filtrados_regic, lat = ~ lat, lng = ~ lon, intensity = ~(prev-min(prev))/(max(prev)-min(prev))*5000,
+    addHeatmap(data = casos_mun_db_map, lat = ~ lat, lng = ~ lon, 
+               # intensity = ~(prev-min(prev))/(max(prev)-min(prev))*50000,
+               intensity = ~prev*5000,
                radius=25,
                blur=20,
                minOpacity=0.01,
